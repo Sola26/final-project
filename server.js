472080b4c76f4bca5dce797fff08fb1c68dd0ffa
Sola26 <42605615+Sola26@users.noreply.github.com>
2018-11-15 14:20:55 +0100

    f

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require("express");
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname + "/dist/"));
+app.use(express.static(__dirname + "/static"));
 app.get("*", function(req, res) {
-  res.sendfile(__dirname + "/dist/index.html");
+  console.log("req.url", req.url);
+  res.sendfile(__dirname + "/index.html");
 });
 
 app.listen(port);
